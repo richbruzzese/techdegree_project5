4e9927b28927ad928b0a4cdf6f477885cf03fd83
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-07-22 11:46:13 -0400

    Display content
    Functions written to handle:
    - fetching content from API
    - adding image to page
    - adding user data to card

--- js/app.js
+++ js/app.js
@@ -1,10 +1,51 @@
 const search = document.querySelector('.search-container')
-const employeeItem = document.getElementById('gallery')
+const imgCard = document.querySelector('.card-img-container')
+const cardInfo = document.querySelector('.card-info-container')
+const promises = []
 
 function fetchData(url){
     return fetch(url)
         .then(res => res.json())
-        .then (data => console.log(data.results[0].name))
         .catch(err => console.log('Issue fetching data', err))
 }
+
 fetchData('https://randomuser.me/api/')
+    .then(data =>{
+    makeImage(data)
+    makeCard(data)
+    })
+
+// appendData()
+
+/**
+ * Functions
+ */
+
+function makeImage(data){
+    const results = data.results[0]
+    const picture = results.picture.medium
+    const html = `
+    <img class="card-img" src="${picture}" alt="profile photo">
+    `
+
+    imgCard.innerHTML = html
+
+}
+function makeCard(data){
+    console.log(data)
+    const result = data.results[0]
+    const fName  = result.name.first
+    const lName = result.name.last
+    const email = result.email
+    const city = result.location.city
+    const state = result.location.state
+    const html = `
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${lName}, ${fName}</h3>
+            <p class="card-text">${email}</p>
+            <p class="card-text cap">${city}, ${state}</p>
+        </div>
+    `
+
+    cardInfo.innerHTML = html
+}
