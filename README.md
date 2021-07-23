9e6d4436200ff82a4fceab0158fac5f87f3abf16
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-07-23 14:26:03 -0400

    Final changes
    - README updated
    - comments added to code
    - functionality to cycle to modal updated.
    -CSS changes

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # techdegree_project5
  Building an Employee Directory
+
+Employee Directory using Random User API.  Generates a random list of 12 employees.
+Clicking employee displays a modal with more information on the employee
+Function to toggle between employees enabled
+
+Search function will display modal for the employee item.
+
+-- Personalized changes --
+Updated background
+added hover styles to the close button
+Changed the Title of the page
+Modal HTML included in the index.html file and style.display = 'none' by default.
+
+# fetchData
+Function will pull data and parse into JSON, and place data into the employeeData array
+
+# formatPhone
+Parses phone numbers into desired format
+
+# employeeCard
+Loops through and creates div card elements on the page for each of the random employees.  Adds a listener to display a modal on click
+
+# displayModal
+Determines the correct employee to display replaces the HTML content of the modal
--- js/app.js
+++ js/app.js
@@ -1,22 +1,41 @@
+/**
+ * Dom Elements
+ */
 const search = document.querySelector('.search-container')
 const imgCard = document.querySelector('.card-img-container')
 const cardInfo = document.querySelector('.card-info-container')
 const gallery = document.querySelector('.gallery')
 const searchError = document.querySelector('#error')
+const modal = document.querySelector('.modal-container')
+const modalCloseButton = document.querySelector('#modal-close-btn')
+const modalPrev = document.querySelector('#modal-prev')
+const modalNext = document.querySelector('#modal-next')
+
+//Array of employee data fetched
 let employeeData = []
 
+//Get data from random user API
+fetchData('https://randomuser.me/api/?results=12&nat=us')      
+    .then(employeeCard)
+
+/**
+ * 
+ * @param {*} url to fetch
+ * @returns json data and stores into employeeData array
+ */
 function fetchData(url){
     return fetch(url)
         .then(res => res.json())
         .then(data => employeeData = data)
         .catch(err => console.log('Issue fetching data', err))
 }
-        fetchData('https://randomuser.me/api/?results=12&nat=us')
-            .then(employeeCard)
-/**
- * Functions
- */
 
+
+/**
+ * 
+ * @param {*} str passed is phone number object from employeeData
+ * @returns formatted phone number in (xxx) xxx-xxxx format
+ */
 function formatPhone(str){
     //Filter only numbers from the input
     let cleaned = ('' + str).replace(/\D/g, '')
@@ -27,7 +46,11 @@ function formatPhone(str){
     return null
   }
 
-
+/**
+ * 
+ * @param {*} data passed is employeeData pulled from fetch request
+ * Function will create and insert div elements onto the page for each employee
+ */
 function employeeCard(data){
     for(let i = 0; i< data.results.length; i++){
         let employee = data.results[i]
@@ -41,7 +64,7 @@ function employeeCard(data){
         </div>
 
         <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${employee.name.first}, ${employee.name.last}</h3>
+            <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
             <p class="card-text">${employee.email}</p>
             <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
             </div>
@@ -51,19 +74,29 @@ function employeeCard(data){
 
 }
 
+/**
+ * 
+ * @param {*} employee is employee item to display in the modal windo
+ * @param {*} index is the current index value to allow to cycle through employee objects
+ */
 function displayModal(employee, index){
     let card = index
     const location = employee.location
     const streetInfo = location.street
     let date = new Date(employee.dob.date)
     let phone = formatPhone(employee.phone)
-    
+    modal.style.display = 'flex'    
+        
+    // Only display buttons for previous and next in modal if they are not the first and last items
+        if(card === 0){modalPrev.style.display = 'none'}else{
+        modalPrev.style.display = 'inline-block'
+        }
+        if(card === 11){modalNext.style.display = 'none'}else{
+        modalNext.style.display = 'inline-block'
+        }
 
-    document.body.insertAdjacentHTML('beforeend',`
-    <div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
+    // Replace the elements within the modal inner html
+    modal.firstElementChild.firstElementChild.nextElementSibling.innerHTML = `
             <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
             <h3 id="name" class="modal-name cap">${employee.name.first}, ${employee.name.last}</h3>
             <p class="modal-text">${employee.email}</p>
@@ -71,24 +104,33 @@ function displayModal(employee, index){
             <hr>
             <p class="modal-text">${phone}</p>
             <p class="modal-text">${streetInfo.number} ${streetInfo.name}, ${location.city}, ${location.state} ${location.postcode}</p>
-            <p class="modal-text">${date.getMonth()}/${date.getDay()}/${date.getFullYear()}</p>
-        </div>
-        <div class="modal-btn-container">
-                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
-                </div>
-            </div>
-    </div>    
-    `)
-    let modalButton = document.querySelector('#modal-close-btn')
-    modalButton.addEventListener('click', () => {
-        modalButton.parentNode.parentNode.remove()
+            <p class="modal-text"><b>Birthday</b>: ${date.getUTCMonth()}/${date.getUTCDate()}/${date.getUTCFullYear()}</p>
+    `
+    //Event Listeners for closing the modal, viewing previous, and viewing next
+    modalCloseButton.addEventListener('click', () => {
+        modal.style.display = 'none'
         search.firstElementChild.firstElementChild.value = ''
     })
+    modalNext.addEventListener('click', () =>{
+        if(card < 11){
+            card++
+            displayModal(employeeData.results[card], card)
+        }
+    })
+    modalPrev.addEventListener('click', () =>{
+        if(card > 0){
+            card--
+            displayModal(employeeData.results[card], card)
+        }
+    })
+    
 
 
 }
-
+/**
+ * Event listener for search bar.  Will display the modal for the matched search.
+ * If no match, display an error that employee not found
+ */
 search.addEventListener('submit', () =>{
     let searchInput = search.firstElementChild.firstElementChild.value.toLowerCase();
     console.log(searchInput)
@@ -96,11 +138,12 @@ search.addEventListener('submit', () =>{
         let fName = employeeData.results[i].name.first.toLowerCase()
         let lName = employeeData.results[i].name.last.toLowerCase()
         if(fName.includes(searchInput) || lName.includes(searchInput)){
-                displayModal(employeeData.results[i])
+                displayModal(employeeData.results[i], i)
                 searchError.innerHTML=''
                 break;
             }else{
                 searchError.innerHTML = 'Employee not found'
+                setTimeout(() => searchError.innerHTML = '', 3000)
         }
     }
     
