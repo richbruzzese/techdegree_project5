af263c9550dfbf85879637f5930cb41c75ed00c9
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-07-24 11:48:06 -0400

    Updates after feedback
    - created functions to add the modal and the search bar when fetching the data.
    - updated event listeners
    - added new listener to filter elements on keyup

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
 /**
  * Dom Elements
  */
-const search = document.querySelector('.search-container')
 const imgCard = document.querySelector('.card-img-container')
 const cardInfo = document.querySelector('.card-info-container')
 const gallery = document.querySelector('.gallery')
-const searchError = document.querySelector('#error')
-const modal = document.querySelector('.modal-container')
-const modalCloseButton = document.querySelector('#modal-close-btn')
-const modalPrev = document.querySelector('#modal-prev')
-const modalNext = document.querySelector('#modal-next')
 
 //Array of employee data fetched
 let employeeData = []
@@ -17,6 +11,8 @@ let employeeData = []
 //Get data from random user API
 fetchData('https://randomuser.me/api/?results=12&nat=us')      
     .then(employeeCard)
+    .then(createModal)
+    .then(createSearchBar)
 
 /**
  * 
@@ -51,6 +47,71 @@ function formatPhone(str){
  * @param {*} data passed is employeeData pulled from fetch request
  * Function will create and insert div elements onto the page for each employee
  */
+function createModal(){
+    const newModal = document.createElement('div')
+    newModal.classList.add('modal-container')
+    gallery.appendChild(newModal)
+    newModal.insertAdjacentHTML('beforeend',`
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+    </div>
+        <div class="modal-btn-container">
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>
+    `)
+}
+function createSearchBar(){
+    const searchBar = document.createElement('div')
+    const header = document.querySelector('.header-inner-container')
+    searchBar.classList.add('search-container')
+    header.appendChild(searchBar)
+    searchBar.insertAdjacentHTML('beforeend',`
+    <form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+         <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>
+    <span id="error" style="color: red"></span>
+    `)
+    const search = document.querySelector('.search-container')
+    const searchError = document.querySelector('#error')
+
+    /**
+ * Event listener for search bar.  Will display the modal for the matched search.
+ * If no match, display an error that employee not found
+ */ 
+    search.addEventListener('keyup', ()=>{
+        let searchInput = search.firstElementChild.firstElementChild.value.toLowerCase();
+        console.log(searchInput)
+        for(i=0; i<employeeData.results.length; i++){
+            let names = gallery.children[i].children[1].children[0].textContent.toLowerCase()
+            if(names.includes(searchInput)){
+                gallery.children[i].setAttribute('style', 'display:flex')
+            }else if(!names.includes(searchInput)){
+                gallery.children[i].setAttribute('style', 'display:none')
+            }           
+        }
+    })
+    search.addEventListener('submit', () =>{
+     let searchInput = search.firstElementChild.firstElementChild.value.toLowerCase();
+     
+     for(let i = 0; i< employeeData.results.length; i++){
+        let fName = employeeData.results[i].name.first.toLowerCase()
+        let lName = employeeData.results[i].name.last.toLowerCase()
+        if(fName.includes(searchInput) || lName.includes(searchInput)){
+                displayModal(employeeData.results[i], i)
+                searchError.innerHTML=''
+                break;
+            }else{
+                searchError.innerHTML = 'Employee not found'
+                setTimeout(() => searchError.innerHTML = '', 3000)
+        }
+    }
+    
+})
+}
 function employeeCard(data){
     for(let i = 0; i< data.results.length; i++){
         let employee = data.results[i]
@@ -85,20 +146,24 @@ function displayModal(employee, index){
     const streetInfo = location.street
     let date = new Date(employee.dob.date)
     let phone = formatPhone(employee.phone)
+    const modal = document.querySelector('.modal-container')
+    const modalCloseButton = document.querySelector('#modal-close-btn')
+    const modalPrev = document.querySelector('#modal-prev')
+    const modalNext = document.querySelector('#modal-next')
     modal.style.display = 'flex'    
         
     // Only display buttons for previous and next in modal if they are not the first and last items
         if(card === 0){modalPrev.style.display = 'none'}else{
-        modalPrev.style.display = 'inline-block'
+         modalPrev.style.display = 'inline-block'
         }
         if(card === 11){modalNext.style.display = 'none'}else{
-        modalNext.style.display = 'inline-block'
+         modalNext.style.display = 'inline-block'
         }
 
     // Replace the elements within the modal inner html
     modal.firstElementChild.firstElementChild.nextElementSibling.innerHTML = `
             <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">${employee.name.first}, ${employee.name.last}</h3>
+            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
             <p class="modal-text">${employee.email}</p>
             <p class="modal-text cap">${location.city}</p>
             <hr>
@@ -108,8 +173,12 @@ function displayModal(employee, index){
     `
     //Event Listeners for closing the modal, viewing previous, and viewing next
     modalCloseButton.addEventListener('click', () => {
+        const search = document.querySelector('.search-container')
         modal.style.display = 'none'
         search.firstElementChild.firstElementChild.value = ''
+        for(i = 0; i<employeeData.results.length; i++){
+            gallery.children[i].setAttribute('style', 'display:flex')
+        }
     })
     modalNext.addEventListener('click', () =>{
         if(card < 11){
@@ -127,24 +196,3 @@ function displayModal(employee, index){
 
 
 }
-/**
- * Event listener for search bar.  Will display the modal for the matched search.
- * If no match, display an error that employee not found
- */
-search.addEventListener('submit', () =>{
-    let searchInput = search.firstElementChild.firstElementChild.value.toLowerCase();
-    console.log(searchInput)
-    for(let i = 0; i< employeeData.results.length; i++){
-        let fName = employeeData.results[i].name.first.toLowerCase()
-        let lName = employeeData.results[i].name.last.toLowerCase()
-        if(fName.includes(searchInput) || lName.includes(searchInput)){
-                displayModal(employeeData.results[i], i)
-                searchError.innerHTML=''
-                break;
-            }else{
-                searchError.innerHTML = 'Employee not found'
-                setTimeout(() => searchError.innerHTML = '', 3000)
-        }
-    }
-    
-})
